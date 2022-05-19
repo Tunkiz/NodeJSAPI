604ea768cd5c754c48874248574ebee4fe92ec45
Tunkiz <18015037@mvula.univen.ac.za>
2022-05-19 14:11:45 +0200

    Update

---
+++ app/models/db.js
@@ -0,0 +1,19 @@
+//Define the database model
+const mysql = require('mysql');
+const dbConfig = require('../config/db.config');
+//Create a connection to the database
+const connection = mysql.createConnection(
+    {
+    host: dbConfig.HOST,
+    user: dbConfig.USER,
+    password: dbConfig.PASSWORD,
+    database: dbConfig.DB
+    }
+);
+
+//open mysql connection
+connection.connect(error => {
+    if(error) throw error;
+    console.log('Database connection successfull');
+});
+module.exports = connection;
--- server.js
+++ server.js
@@ -1,8 +1,41 @@
-var express = require('express');
+const express =  require('express');
 var cors = require('cors');
+const res = require('express/lib/response')
 var app = express();
-//CORS stands for Cross
-var corOptions = {
-    origin: "http:/localhost:8081"
+
+
+var corsOption = {
+    origin: 'http://localhost:8081'
 }
-console.log('Our project is running');
+console.log("is our server running");
+app.use(cors(corsOption))
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+
+//simple route
+
+app.get('/', (req, res)=>{
+    res.json({
+        message : 'Welcome to the World of Node JS'
+    });
+});
+
+require("./app/routes/tutorials.routes.js");
+const PORT = process.env.PORT || 8081;
+app.listen(PORT, ()=>{
+    console.log(`server running on port ${PORT}`);
+    console.log('http://localhost:8081');
+});
+
+//  Create DataBass NodeJSAPI
+//  Create MySQL table tutorials
+//  Create Table if not exists tutorials (
+//  id int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+//  tittle varchar(225) NOT NULL
+//  description varchar(2225),
+//  published BOOLESN DEFAULT false,    
+//) 
+
+//Configure & connect our application to MySQL database
+require('./app/models/db');
